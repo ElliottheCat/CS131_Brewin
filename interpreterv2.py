31f2ac2c4455b6c14466758531b2f406d6da3787
Elliot cat <xiyuan23@g.ucla.edu>
2025-10-27 22:03:56 -0700

    fixed equal logic

--- interpreterv2.py
+++ interpreterv2.py
@@ -44,6 +44,7 @@ class Interpreter(InterpreterBase):
         # self.scope_stack=[("main",0,self.env)] #including the just created environment # we don' tneed scope stack anymore. we are keeping track of scope staically inside the FCALL funciton
         self.return_stack=[]
         self.set_return=False
+        self.something_to_return=False
         self.user_function_def : Dict[Tuple[str,int],Element] = {} # (name, arg#): element.
 
         self.integer_ops_bi = {"-", "/","*"} # NOTE: use // for integer division and truncation in python. 
@@ -92,6 +93,7 @@ class Interpreter(InterpreterBase):
 
     def run_func(self, func_node:Element):
         self.set_return=False
+        self.something_to_return=False
         name=func_node.get('name')
         args=len(func_node.get('args')) #type: ignore
         scope=(name,args)
@@ -115,7 +117,7 @@ class Interpreter(InterpreterBase):
         elif kind=='=':
             self.assign_statement(statement_node)
         elif kind==self.FCALL_NODE:
-            self.func_call_statement(statement_node)
+            return self.func_call_statement(statement_node) # return the value!
         elif kind==self.IF_NODE:
             self.if_statement_execution(statement_node)
         elif kind==self.WHILE_NODE:
@@ -246,7 +248,7 @@ class Interpreter(InterpreterBase):
             self.scope=last_scope
             self.env=last_env
             
-            if self.set_return:
+            if self.something_to_return:
                 return self.return_stack.pop()
             else:
                 return None
@@ -350,12 +352,15 @@ class Interpreter(InterpreterBase):
             t2 = type(op2)
             
             equal=True
-            if t1 != t2:
-                equal= False
-            elif op1 == None and op2==None: # just a special case I want to make sure it works
+            if op1 == None and op2==None: # just a special case I want to make sure it works
                 equal=True
+            elif op1 ==None or op2==None:
+                return False
+            elif t1 != t2:
+                equal= False
+            else:
             # everything should be the same type at this point: 
-            equal = op1 == op2
+                equal = (op1 == op2)
 
             match kind:
                 case "==":
@@ -422,9 +427,11 @@ class Interpreter(InterpreterBase):
 
         if rtr==None:
             self.set_return=True
+            self.something_to_return=False
             return
         else:
             self.set_return=True
+            self.something_to_return=True
             self.return_stack.append(rtr)#type: ignore
             return
     
